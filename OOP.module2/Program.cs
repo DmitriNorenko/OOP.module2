9124aa86454816e201261385591dee6dbc5909e3
Name <email@exaple.ru>
2023-06-13 12:14:05 +0300

    7.1.4

--- OOP.module2/Program.cs
+++ OOP.module2/Program.cs
@@ -11,5 +11,20 @@ namespace OOP.module2
         static void Main(string[] args)
         {
         }
+        class Employee
+        {
+            public string Name;
+            public int Age;
+            public int Salary;
+        }
+        class ProjectManager : Employee
+        {
+            public string ProjectName;
+        }
+        class Developer : Employee
+        {
+            public string ProgrammingLanguage;
+        }
+
     }
 }
