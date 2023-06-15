a04cb89f92e59f8c35bb5acfc201f447ab0dce78
Name <email@exaple.ru>
2023-06-15 09:59:00 +0300

    7.2.7

--- OOP.module2/Program.cs
+++ OOP.module2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
@@ -11,27 +12,13 @@ namespace OOP.module2
     {
         static void Main(string[] args)
         {
-            BaseClass baseClass = new BaseClass();
-            DerivedClass derivedClass = new DerivedClass();
-            baseClass.Display();
-            derivedClass.Display();
-        }
-        class BaseClass
-        {
-            public virtual void Display()
-            {
-                Console.WriteLine("Метод класса BaseClass");
-            }
-        }
 
-        class DerivedClass : BaseClass
-        {
-            public override void Display()
-
-            {
-                base.Display();
-                Console.WriteLine("Метод класса DerivedClass");
-            }
         }
+        class A { public virtual void Display() { Console.WriteLine("A"); } }
+        class B : A { public new void Display() { Console.WriteLine("B"); } }
+        class C : A { public override void Display() { Console.WriteLine("C"); } }
+        class D : B { public new void Display() { Console.WriteLine("D"); } }
+        class E : C { public new void Display() { Console.WriteLine("E"); } }
+
     }
 }
