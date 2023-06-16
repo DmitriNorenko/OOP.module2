ae8ce4322a7486ad9d9ec41ed85848208d6ff7f1
Name <email@exaple.ru>
2023-06-16 07:22:09 +0300

    7.5.3

--- OOP.module2/Program.cs
+++ OOP.module2/Program.cs
@@ -12,13 +12,21 @@ namespace OOP.module2
     {
         static void Main(string[] args)
         {
-
+            int num1 = 34;
+            int num2 = 56;
+            Helper.Swap(ref num1, ref num2);
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
         }
-        public class Obj
+        class Helper
         {
-            public string Name;
-            public string Description;
-            public static int MaxValue = 2000;
+
+            public static void Swap(ref int a, ref int b)
+            {
+                int d = a;
+                a = b;
+                b = d;
+            }
         }
     }
 
