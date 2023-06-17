b8899d5a420408d81e415a0308809e0fd3d8e9ec
Name <email@exaple.ru>
2023-06-17 06:32:06 +0300

    7.6.6

--- OOP.module2/Program.cs
+++ OOP.module2/Program.cs
@@ -14,13 +14,13 @@ namespace OOP.module2
         {
 
         }
-        class Car<T>
+        class Record<T1, T2>
         {
-            public T Engine;
+            public T1 Id;
+            public T2 Value;
+            public DateTime Date;
         }
-        class ElectricEngine { }
-        class GasEngine { }
     }
 }
 
-}
+
